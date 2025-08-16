8220f16e25b4f4c6adff9e7ef7ac8d7313b26423
Sergey Kupriyanov <xvoztuk@gmail.com>
2025-08-17 00:59:42 +0400

    add integration test, not fully implemented yet

---
+++ tests/health_check.rs
@@ -0,0 +1,23 @@
+use reqwest;
+
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    spawn_app().await.expect("Failed to spawn app");
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get("http://127.0.0.1:8080/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+async fn spawn_app() -> std::io::Result<()> {
+    zero2prod_newsletter::run().await
+}
