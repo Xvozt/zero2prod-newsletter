7d93242d060e56bf9f1e4c3f421f6062f4b5312b
Sergey Kupriyanov <xvoztuk@gmail.com>
2025-08-28 01:18:56 +0400

    extracted telemetry initialization logic to separate module

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,13 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::{EnvFilter, Registry};
+use zero2prod_newsletter::telemetry;
 use zero2prod_newsletter::configuration::get_config;
 use zero2prod_newsletter::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    LogTracer::init().expect("Failed to set up logger");
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = telemetry::get_subscriber("zero2prod".into(), "info".into());
+    telemetry::init_subscriber(subscriber);
 
     let configuration = get_config().expect("Failed to read configuration");
 
@@ -29,3 +19,4 @@ async fn main() -> std::io::Result<()> {
     run(listener, connection_pool)?.await?;
     Ok(())
 }
+
