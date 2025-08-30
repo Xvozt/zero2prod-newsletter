64674e90b27265daaf599e418c0b17519fadfc4a
Sergey Kupriyanov <xvoztuk@gmail.com>
2025-08-30 17:19:35 +0400

    cleaned-up instrumentation logic, added instrument macro, extrated logic of saving subscriber to teh database

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,51 +1,58 @@
 use actix_web::{post, web, HttpResponse};
+use actix_web::web::Data;
 use chrono::Utc;
-use tracing::{self, Instrument};
+use tracing::{self};
 use serde::Deserialize;
 use sqlx::PgPool;
 use uuid::Uuid;
 
 #[derive(Deserialize)]
-struct FormData {
+pub struct FormData {
     email: String,
     name: String,
 }
-
+#[tracing::instrument(
+name = "Adding a subscriber",
+skip(form, pool),
+fields(
+    request_id = %Uuid::new_v4(),
+    subscriber_email = %form.email,
+    subscriber_name = %form.name
+    )
+)]
 #[post("/subscriptions")]
-async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!(
-        "Adding a subscriber.",
-        %request_id,
-        subscriber_email = %form.email,
-        subscriber_name = %form.name
-    );
-    let _request_span_guard = request_span.enter();
-    let user_id = Uuid::new_v4();
-    let query_span = tracing::info_span!(
-        "Saving new subscriber in the database"
-    );
-    match sqlx::query!(
-        r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1, $2, $3, $4 )
-        "#,
-        user_id,
-        form.email,
-        form.name,
-        Utc::now()
-    ).execute(pool.get_ref())
-        .instrument(query_span)
-        .await
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    match insert_subscriber(&pool, &form).await
     {
         Ok(_) => {
-            tracing::info!("request_id {} - New sub '{}' was successfully saved with id: '{}'", request_id, form.name, user_id);
             HttpResponse::Ok().finish()
         },
-        Err(e) => {
-            tracing::error!("request_id {} - Failed to execute query: {:?}", request_id, e);
+        Err(_) => {
             HttpResponse::InternalServerError().finish()
         }
     }
 
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(pool: &Data<PgPool>, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4 )
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    ).execute(pool.get_ref())
+        .await
+        .map_err(|e| {
+            tracing::error!("Failed to execute query: {:?}", e);
+            e
+        })?;
+    Ok(())
 }
