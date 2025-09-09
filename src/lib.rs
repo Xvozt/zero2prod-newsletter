97b12d11af9a9574113daaa9efc4eaef0e38571b
s.v.kupriyanov <s.v.kupriyanov@tinkoff.ru>
2025-09-09 23:43:19 +0400

    extracted dev dependencies, add email client mod

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,5 @@ pub mod domain;
 pub mod routes;
 pub mod startup;
 pub mod telemetry;
+
+pub mod email_client;
--- src/startup.rs
+++ src/startup.rs
@@ -3,16 +3,19 @@ use actix_web::dev;
 use std::net::TcpListener;
 use sqlx::PgPool;
 use tracing_actix_web::TracingLogger;
+use crate::email_client::EmailClient;
 use crate::routes::{health_check, subscribe};
 
-pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<dev::Server, std::io::Error> {
+pub fn run(listener: TcpListener, db_pool: PgPool, email_client: EmailClient) -> Result<dev::Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
+    let email_client = web::Data::new(email_client);
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
             .service(health_check)
             .service(subscribe)
             .app_data(db_pool.clone())
+            .app_data(email_client.clone())
     })
         .listen(listener)?
         .run();
