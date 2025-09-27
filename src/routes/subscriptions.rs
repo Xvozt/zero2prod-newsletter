42a92191fcf43765c41e01c111ad5184346b5e60
Sergey Kupriyanov <xvoztuk@gmail.com>
2025-09-27 23:02:03 +0400

    preparing for generating token, add new field to db, optional for now

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,5 @@
-use actix_web::{post, HttpResponse};
-use actix_web::web::{Form, Data};
+use actix_web::web::{Data, Form};
+use actix_web::{HttpResponse, post};
 use chrono::Utc;
 use serde::Deserialize;
 use sqlx::PgPool;
@@ -26,7 +26,7 @@ fields(
 pub async fn subscribe(form: Form<FormData>, pool: Data<PgPool>) -> HttpResponse {
     let new_subscriber = match form.0.try_into() {
         Ok(subscriber) => subscriber,
-        Err(_) => return HttpResponse::BadRequest().finish()
+        Err(_) => return HttpResponse::BadRequest().finish(),
     };
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
@@ -44,13 +44,14 @@ pub async fn insert_subscriber(
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1, $2, $3, $4 )
+        INSERT INTO subscriptions (id, email, name, subscribed_at, status)
+        VALUES ($1, $2, $3, $4, $5)
         "#,
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
-        Utc::now()
+        Utc::now(),
+        "confirmed",
     )
     .execute(pool.get_ref())
     .await
