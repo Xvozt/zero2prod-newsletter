e1baef18fb0a7d9d9a07b76b59e6742df873033e
Sergey Kupriyanov <xvoztuk@gmail.com>
2025-09-27 23:15:42 +0400

    db migration

---
+++ migrations/20250927190547_make_status_not_null_in_subscriptions.sql
@@ -0,0 +1,6 @@
+-- Add migration script here
+BEGIN;
+    UPDATE subscriptions
+        SET status = 'confirmed'
+        WHERE STATUS IS NULL;
+    ALTER TABLE subscriptions ALTER COLUMN status SET NOT NULL;
