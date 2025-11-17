5b4e1e9722b342024987f6ecfcd23454d8595d85
Sergey Kupriyanov <xvoztuk@gmail.com>
2025-11-17 10:49:26 +0400

    add body data to construct newsletter

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,6 +1,19 @@
-use actix_web::{post, HttpResponse};
+use actix_web::{post, web, HttpResponse};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct BodyData {
+    title: String,
+    content: Content,
+}
+
+#[derive(Deserialize)]
+pub struct Content {
+    html: String,
+    text: String,
+}
 
 #[post("/newsletters")]
-pub async fn publish_newsletter() -> HttpResponse {
+pub async fn publish_newsletter(_body: web::Json<BodyData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
