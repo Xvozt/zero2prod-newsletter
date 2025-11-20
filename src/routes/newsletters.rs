ff710f169b63616b6eb74d7ecc0fca73f984a30e
s.v.kupriyanov <s.v.kupriyanov@tinkoff.ru>
2025-11-20 12:02:57 +0400

    refactoring: got rid of struct for row, returned to sqlx::query

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -80,26 +80,21 @@ pub async fn publish_newsletter(
 async fn get_confirmed_subscribers(
     pool: &PgPool,
 ) -> Result<Vec<Result<ConfirmedSubscriber, anyhow::Error>>, anyhow::Error> {
-    struct Row {
-        email: String,
-    }
-
-    let rows = sqlx::query_as!(
-        Row,
+    let confirmed_subscribers = sqlx::query!(
         r#"
-        SELECT email from subscriptions
+        SELECT email
+        FROM subscriptions
         WHERE status = 'confirmed'
-        "#,
+        "#
     )
     .fetch_all(pool)
-    .await?;
+    .await?
+    .into_iter()
+    .map(|r| match SubscriberEmail::parse(r.email) {
+        Ok(email) => Ok(ConfirmedSubscriber { email }),
+        Err(error) => Err(anyhow::anyhow!(error)),
+    })
+    .collect();
 
-    let confirmed_subscribers = rows
-        .into_iter()
-        .map(|r| match SubscriberEmail::parse(r.email) {
-            Ok(email) => Ok(ConfirmedSubscriber { email }),
-            Err(error) => Err(anyhow::anyhow!(error)),
-        })
-        .collect();
     Ok(confirmed_subscribers)
 }
